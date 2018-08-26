cd0140051dd97e1c9f3cb53cfdd24f04a4fec1be
Eric Graham <photon0f@protonmail.com>
2018-08-25 23:04:04 -0500

    Add colors

--- ec.c
+++ ec.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+// Define some ANSI color codes
+static const char YELLOW[] = "\x1b[33m", \
+	GREEN[] = "\x1b[32m", \
+	CYAN[] = "\x1b[36m", \
+	BOLD[] = "\x1b[1m", \
+	RESET[] = "\x1b[0m";
+
 // Make the program "logo"
 static const char *eggsAscii = " ..   ..   ..   ..   ..   ..\n"
 			".  . .  . .  . .  . .  . .  .\n"
@@ -10,20 +17,21 @@ static int getIntInput(char* prompt);
 
 int main(void) {
   // Print some info and the "logo"
-  printf("\nEggCounter -- 2018\n%s\n\n", eggsAscii);
+  printf("\nEggCounter -- 2018\n%s%s%s%s\n\n", YELLOW, BOLD, eggsAscii, 
+RESET);
 
   // Provide some context for the user
-  printf("How many egg cartons would you like?\n");
+  printf("%sHow many egg cartons would you like?\n", CYAN);
 
   int twelvePacks = getIntInput("Normal Cartons (12 eggs): ");
   int eighteenPacks = getIntInput("Large Cartons (18 eggs): ");
   int totalEggCount = twelvePacks * 12 + eighteenPacks * 18;
 
-  printf("\n\tYour Order\n");
+  printf("%s\n\tYour Order\n", RESET);
   printf("--------------------------\n");
   printf("12 Packs: %d (%d eggs)\n", twelvePacks, twelvePacks * 12);
   printf("18 Packs: %d (%d eggs)\n", eighteenPacks, eighteenPacks * 18);
-  printf("   Total: %d eggs\n", totalEggCount);
+  printf("   Total: %s%d eggs%s\n", GREEN, totalEggCount, RESET);
 
   // I had planned to grab the current egg price from some kind of grocery api,
   // but a lot of them charge obscene prices or require a lot of work signing
@@ -32,7 +40,8 @@ int main(void) {
   float eggPrice = 2.67; // Derived from the price of a dozen on Amazon
 
   printf("--------------------------\n");
-  printf("Total Price (excl. tax): $%.2f ($%.2f / egg)\n\n", totalEggCount * eggPrice, 
+  printf("Total Price (excl. tax): %s$%.2f%s ($%.2f / egg)\n\n", 
+GREEN, totalEggCount * eggPrice, RESET, 
 eggPrice);
 }
 
